f51326b4a8f34968ab708b2ad1d79a0ab498a3cb
FHHIZ <hafizh.raissan@gmail.com>
2026-02-06 09:10:24 +0700

    development template v2

--- src/app/(authenticated)/profile/page.tsx
+++ src/app/(authenticated)/profile/page.tsx
@@ -1,5 +1,5 @@
 "use client";
 
-export default function Absence() {
+export default function Profile() {
   return <div className="flex justify-center items-center"></div>;
 }
--- src/app/(authenticated)/reset-password/page.tsx
+++ src/app/(authenticated)/reset-password/page.tsx
@@ -1,5 +1,5 @@
 "use client";
 
-export default function Absence() {
+export default function resetPassword() {
   return <div className="flex justify-center items-center"></div>;
 }
