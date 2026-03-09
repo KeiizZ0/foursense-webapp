de599cfc187a009aee79a668fc8b9b37f7cd9095
FHHIZ <hafizh.raissan@gmail.com>
2026-03-09 08:40:16 +0700

    abc

--- src/service/user/user.api.ts
+++ src/service/user/user.api.ts
@@ -1,13 +1,16 @@
-"use server";
+"use client";
 
+import { getCookie } from "@/lib/helpers/cookies";
 import { ShowMeRes } from "./user.type";
 
 export async function ShowMeAPI() {
+  const key = await getCookie("acctkn");
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user/me`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       "ngrok-skip-browser-warning": "true",
+      Authorization: `Bearer ${key}`,
     },
   });
 
